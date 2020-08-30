ec1331981fee1ead7d2c683ff38b94d47055276d
Zanetta Norris <zanettanorris@gmail.com>
2020-08-30 01:21:25 -0400

    Added viewcontroller class and view index.html

---
+++ src/main/java/romaniancoder/booking/ViewController.java
@@ -0,0 +1,12 @@
+package romaniancoder.booking;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ViewController {
+    @RequestMapping("/")
+    public String index (){
+        return "index";
+    }
+}
---
+++ src/main/resources/static/bookings.controller.js
@@ -0,0 +1,10 @@
+(function(){
+    'use strict';
+    angular
+        .module('app')
+        .controller('BookingsController', BookingsController);
+    BookingsController.$inject = []
+    function BookingsController(){
+        var vm =this;
+    }
+})();
